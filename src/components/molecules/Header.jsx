7451d5c41e50263b967a4a99f5d8676c9c17a633
MrIceTea321 <info@maximilian-drescher.de>
2024-10-08 10:23:50 +0200

    refactor(components): :zap: improved image loading

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -1,47 +1,10 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import LoadingIndicator from '../atoms/LoadingIndicator'
+import logo from '../../../assets/ffwlogo.webp'
 
 function Header() {
     const navigate = useNavigate()
     const location = useLocation()
-    const [loadingImage, setLoadingImage] = useState(true)
-    const [imageSrc, setImageSrc] = useState(null)
-    const IMAGE_URL =
-        'https://mipbtnsxhpoikpsltmtr.supabase.co/storage/v1/object/public/Weininsel-Wehr/ffwlogo.webp'
-    const CACHE_KEY = 'headerLogoCache'
-
-    // Cache the image in local storage
-    const cacheImage = (imageURL) => {
-        sessionStorage.setItem(
-            CACHE_KEY,
-            JSON.stringify({ src: imageURL, timestamp: Date.now() })
-        )
-    }
-
-    useEffect(() => {
-        const cached = sessionStorage.getItem(CACHE_KEY)
-
-        if (!cached) {
-            // If no valid cache, load the image and cache it
-            const img = new Image()
-            img.src = IMAGE_URL
-            img.onload = () => {
-                setImageSrc(IMAGE_URL)
-                setLoadingImage(false)
-                cacheImage(IMAGE_URL)
-            }
-            img.onerror = (err) => {
-                console.error('Error loading image:', err)
-                setLoadingImage(false)
-            }
-        } else {
-            const { src } = JSON.parse(cached)
-            setImageSrc(src)
-            setLoadingImage(false)
-        }
-    }, [])
 
     return (
         <header className="shadow-lg mb-4 py-1 lg:py-4 bg-green transition-colors">
@@ -62,18 +25,11 @@ function Header() {
 
                     <Link to="/">
                         <div className="flex-shrink-0">
-                            {loadingImage && (
-                                <div className="flex justify-center py-4">
-                                    <LoadingIndicator />
-                                </div>
-                            )}
-                            {imageSrc && (
-                                <img
-                                    src={imageSrc}
-                                    alt="Logo"
-                                    className={`w-14 opacity-80 object-fit ${loadingImage ? 'hidden' : 'block'}`}
-                                />
-                            )}
+                            <img
+                                src={logo}
+                                alt="Logo"
+                                className={`w-14 opacity-80 object-fit}`}
+                            />
                         </div>
                     </Link>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,13 @@ import LinkButton from '../components/atoms/LInkButton'
 import { useEffect, useState } from 'react'
 import supabase from '../services/supabaseClient'
 import Calendar from '../components/organisms/Calender'
-import LoadingIndicator from '../components/atoms/LoadingIndicator'
+import logo from '../../assets/ffwlogo.webp'
 
 const MainScreen = () => {
     const [homeTopics, setHomeTopics] = useState([])
-    const [loadingImage, setLoadingImage] = useState(true) // For image loading state
-    const [imageSrc, setImageSrc] = useState(null) // State to hold the image source
-    const IMAGE_URL =
-        'https://mipbtnsxhpoikpsltmtr.supabase.co/storage/v1/object/public/Weininsel-Wehr/ffwlogo.webp'
-    const CACHE_KEY = 'mainScreenImageCache'
 
     useEffect(() => {
         getHomeTopics()
-        loadImageFromCache()
     }, [])
 
     async function getHomeTopics() {
@@ -26,39 +20,13 @@ const MainScreen = () => {
         setHomeTopics(data)
     }
 
-    // Function to load the image from session storage or fetch a new one
-    const loadImageFromCache = () => {
-        const cachedImage = sessionStorage.getItem(CACHE_KEY)
-        if (cachedImage) {
-            setImageSrc(cachedImage)
-            setLoadingImage(false)
-        } else {
-            // If there's no valid cache, use the image URL
-            setImageSrc(IMAGE_URL)
-        }
-    }
-
-    const handleImageLoad = () => {
-        setLoadingImage(false)
-        // Cache the loaded image in session storage
-        sessionStorage.setItem(CACHE_KEY, IMAGE_URL)
-    }
-
     return (
         <div className="flex flex-col items-center p-4">
-            {loadingImage && (
-                <div className="flex justify-center py-4">
-                    <LoadingIndicator />
-                </div>
-            )}
-            {imageSrc && (
-                <img
-                    src={imageSrc}
-                    alt="logo firedepartment"
-                    className={`w-3/12 md:w-1/6 lg:w-2/12 xl:w-1/12 opacity-80 h-auto object-cover ${loadingImage ? 'hidden' : 'block'}`}
-                    onLoad={handleImageLoad}
-                />
-            )}
+            <img
+                src={logo}
+                alt="logo firedepartment"
+                className={`w-3/12 md:w-1/6 lg:w-2/12 xl:w-1/12 opacity-80 h-auto object-cover`}
+            />
             <div className="py-6">
                 <Calendar />
             </div>
