d42b7d361c49f1c4d1c5bef546b8d8c3a3b47c7d
MrIceTea321 <info@maximilian-drescher.de>
2024-10-08 17:54:02 +0200

    refactor(components): :recycle: adjusted router from main topics

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -1,21 +1,37 @@
+import { useEffect, useState } from 'react'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import logo from '../../../assets/ffwlogo.webp'
+import { MTA_HREF } from '../../services/topicsHelper'
 
 function Header() {
     const navigate = useNavigate()
     const location = useLocation()
+    const [staticPath, setStaticPath] = useState(false)
+
+    useEffect(() => {
+        if (location.pathname === MTA_HREF) {
+            setStaticPath(true)
+        } else {
+            setStaticPath(false)
+        }
+    }, [location.pathname, logo])
 
     return (
         <header className="shadow-lg mb-4 py-1 lg:py-4 bg-green transition-colors">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center h-16">
                     {location.pathname !== '/' && (
-                        <div
-                            className="flex-shrink-0 cursor-pointer text-black opacity-65"
-                            onClick={() => navigate(-1)}
-                        >
-                            <ArrowBackIcon fontSize="large" />
+                        <div className="flex-shrink-0 cursor-pointer text-black opacity-65">
+                            {staticPath ? (
+                                <Link to={MTA_HREF}>
+                                    <ArrowBackIcon fontSize="large" />
+                                </Link>
+                            ) : (
+                                <div onClick={() => navigate(-1)}>
+                                    <ArrowBackIcon fontSize="large" />
+                                </div>
+                            )}
                         </div>
                     )}
 
@@ -28,7 +44,7 @@ function Header() {
                             <img
                                 src={logo}
                                 alt="Logo"
-                                className={`w-14 opacity-80 object-fit}`}
+                                className="w-14 opacity-80 object-fit"
                             />
                         </div>
                     </Link>
