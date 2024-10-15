f92c2838030560e7656adf45f7990065ac4e3128
MrIceTea321 <info@maximilian-drescher.de>
2024-10-15 15:47:14 +0200

    updated-eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,26 @@
-import js from "@eslint/js";
-import stylistic from "@stylistic/eslint-plugin";
-import react from "eslint-plugin-react";
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import react from 'eslint-plugin-react'
 
-export default [
-  js.configs.recommended,
-  {
+export default {
+    ...js.configs.recommended,
     plugins: {
-      "@stylistic": stylistic,
-      react: react,
+        '@stylistic': stylistic,
+        react: react,
     },
-    files: ["src/**/*.js", "src/**/*.jsx"],
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      "@stylistic/indent": ["error", 2],
+        'no-unused-vars': 'warn',
+        'no-undef': 'warn',
+        '@stylistic/indent': ['error', 2],
     },
     settings: {
-      react: {
-        version: "detect",
-      },
+        react: {
+            version: 'detect',
+        },
     },
-  },
-];
+    overrides: [
+        {
+            files: ['src/**/*.js', 'src/**/*.jsx'],
+        },
+    ],
+}
